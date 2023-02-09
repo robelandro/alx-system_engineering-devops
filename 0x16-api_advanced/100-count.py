3433686e59d80abfb035803778d1a27767d8d5e9
Robel Pop <robelpopandro@gmail.com>
2023-02-09 08:57:23 +0300

    fixed

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,40 +1,57 @@
 #!/usr/bin/python3
-
-"""Module for count_words method."""
+"""100-count
+"""
+import json
+from operator import itemgetter
 
 import requests
 
+BASE_URL = "https://www.reddit.com/r"
+HEADERS = {"User-Agent": "ALX-Bot"}
 
-def count_words(subreddit, word_list, after="", word_dict={}):
-	"""Queries the Reddit API, parses the title of all hot articles,
-	and prints a sorted count of given keywords (case-insensitive,
-	delimited by spaces. Javascript should count as javascript,
-	but java should not)."""
-	url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"}
-	params = {"limit": 100, "after": after}
-	response = requests.get(url, headers=headers, params=params,
-							allow_redirects=False)
-	if response.status_code != 200:
-		return None
-	data = response.json().get("data")
-	after = data.get("after")
-	posts = data.get("children")
-	for post in posts:
-		title = post.get("data").get("title").lower()
-		for word in word_list:
-			word = word.lower()
-			if word in title:
-				if word in word_dict:
-					word_dict[word] += 1
-				else:
-					word_dict[word] = 1
-	if after is not None:
-		return count_words(subreddit, word_list, after, word_dict)
-	else:
-		if len(word_dict) == 0:
-			return None
-		sorted_words = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
-		for word in sorted_words:
-			print("{}: {}".format(word[0], word[1]))
+
+def count_words(subreddit, word_list, after=None, counter=None):
+    """recursively calls the reddit api to get list hot posts and count the
+    given keyword in their title
+    Args:
+        subreddit (str): title of the subreddit
+        word_list (list): list of key words
+        after (str, optional): id of the hot posts list. Defaults to None.
+        counter (list, optional): KeyWord list counter. Defaults to None.
+    """
+    if len(word_list) == 0:
+        return
+
+    if counter is None:
+        counter = {}
+        for word in word_list:
+            if word.lower() not in counter:
+                counter[word.lower()] = 0
+
+    query = "" if after is None else "&after={}".format(after)
+    result = requests.get(
+        "{}/{}/hot.json?limit=100{}".format(BASE_URL, subreddit, query),
+        headers=HEADERS,
+        allow_redirects=False,
+    )
+
+    if result.status_code == 200:
+        data = json.loads(result.text)["data"]
+
+        for post in data["children"]:
+            title = post.get('data').get("title").lower().split()
+            for word in word_list:
+                counter[word.lower()] += sum(w == word.lower() for w in title)
+
+        after = data["after"]
+        if after is None:
+            counter = sorted(
+                sorted(counter.items()),
+                key=itemgetter(1),
+                reverse=True,
+            )
+            for word in counter:
+                if word[1] > 0:
+                    print("{}: {}".format(word[0], word[1]))
+        else:
+            count_words(subreddit, word_list, after, counter)
